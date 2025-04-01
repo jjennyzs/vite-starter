ce16fbab91a4301d15d46dfe0815b8f98c5378e6
JuthathipLoedsinaudom <59654072+jjennyzs@users.noreply.github.com>
2025-04-01 11:48:51 +0900

    Define types for Arrays

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,9 @@ console.log('hello world from Vite');
 
 
 let name: string | undefined | null = 'Jenny';
+
+const myNumberArray: number[] = [1,2,3,4,5];
+const myStringArray: string[] = ["Hello","World"];
+
+const namesArray: string[] = [];
+namesArray.push("Jenny");
