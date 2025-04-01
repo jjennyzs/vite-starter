9f9ca39aa13c8b2567b0b55ce5efc270e1c77c13
JuthathipLoedsinaudom <59654072+jjennyzs@users.noreply.github.com>
2025-04-01 17:29:33 +0900

    TypeScript's ability to infer types from contex

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,39 @@ console.log('hello world from Vite');
 
 let name: string | undefined | null = 'Jenny';
 
-const myNumberArray: number[] = [1,2,3,4,5];
-const myStringArray: string[] = ["Hello","World"];
+const myNumberArray: number[] = [1, 2, 3, 4, 5];
+const myStringArray: string[] = ["Hello", "World"];
 
 const namesArray: string[] = [];
 namesArray.push("Jenny");
 
 
-const myTuple:[string,number] = ["Hello", 123];
+const myTuple: [string, number] = ["Hello", 123];
 
-function printName(name: string){
+function printName(name: string) {
     console.log(`My name is ${name.toUpperCase()}`);
 }
 
 printName('Jenny');
 
-function sum(num1: number, num2:number): string | number{
-    if(num1>0){
-        return 'num1 more than zero'
+function sum(num1: number, num2: number): string | number {
+    if (num1 > 0) {
+        return 'num1 more than zero';
+    } else {
+        return num1 + num2;
     }
-    return num1+num2;
+
 }
 
-const result = sum(1,2);
+const result = sum(1, 2);
+
+
+const names = ['Alice', 'Barbie', 'Christopher'];
+
+names.forEach((name)=>{
+    console.log(`Hello ${name.toUpperCase()}`);
+})
+
+
+
 
